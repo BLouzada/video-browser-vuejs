1cf69cefe928fa9aea4b1d8b1ca1a4c38361ddb7
Bruno Louzada <blouzada@live.com>
2018-04-08 17:30:04 -0300

    search component

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <SearchBar @termChange="handleTerm"/>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar'
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyCbwLwNN-h4LUgIALLOY38_xaQj0Nq_i4Q'
+
+const searchYtVideos = function (term) {
+  return YTSearch({ key: API_KEY, term }, function (videos) {
+    return videos
+  })
+}
+searchYtVideos('surfboards')
+export default {
+  name: 'App',
+  data () {
+    return {
+      videos: []
+    }
+  },
+  components: {
+    SearchBar
+  },
+  methods: {
+    handleTerm: function (payload) {
+      this.videos = searchYtVideos(payload.term)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+ <div className="search-bar">
+    <input v-model="term" v-on:keydown="inputTermChange"/>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+  name: 'SearchBar',
+  data () {
+    return {
+      term: 'surfboards'
+    }
+  },
+  methods: {
+    inputTermChange: _.debounce(function (event) {
+      this.$emit('termChange', { term: this.term })
+    }, 300)
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
